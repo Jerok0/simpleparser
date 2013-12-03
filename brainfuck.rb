fa29c761b919e65efefc6546c664300ac84130af
Jeroko <jero@yopmail.com>
2013-12-04 00:06:56 +0530

    Improved code appearance

--- brainfuck.rb
+++ brainfuck.rb
@@ -8,78 +8,79 @@
 #########################################################################################
 class Brainfuck
 
-	def initialize()
-		@ram = Array.new()
-	end
+  def initialize
+    @ram = Array.new()
+  end
 
-	def newprogram(code, input)
-		@code = code.gsub(/[^><+\-.,\[\]]/, "")
-		@input = input
-		@cp = 0
-		@ip = 0
-		@rm = 0
-		@out = ""
-		initialize
-	end
+  def newprogram(code, input)
+    @code = code.gsub(/[^><+\-.,\[\]]/, "")
+    @input = input
+    @cp = 0
+    @ip = 0
+    @rm = 0
+    @out = ""
+    @ram = Array.new()
+  end
 
-	def getmatch (tofind, original)
-		move = tofind == ']' ? 1 : -1
-		nest = 0
-		while true
-			@cp += move
-			if @code[@cp] == original
-				nest += 1
-			elsif @code[@cp] == tofind
-				nest -= 1
-			end
-			if nest == -1
-				return
-			end
-		end
-	end
+  def getmatch(tofind, original)
+    move = tofind == ']' ? 1 : -1
+    nest = 0
+    while true
+      @cp += move
+      if @code[@cp] == original
+        nest += 1
+      elsif @code[@cp] == tofind
+        nest -= 1
+      end
+      if nest == -1
+        return
+      end
+    end
+  end
 
-	def parse
-		while at = @code[@cp]
-			case at
-			when '>'
-				move(1)
-			when '<'
-				move(-1)
-			when '+'
-				increment
-			when '-'
-				decrement
-			when '.'
-				@out += @ram[@rm].chr
-			when ','
-				@ram[@rm] = @input[@ip].ord
-				@ip += 1
-			when '['
-				getmatch(']', '[') if @ram[@rm] == 0
+  def parse
+    while at = @code[@cp]
+      case at
+      when '>'
+        move(1)
+      when '<'
+        move(-1)
+      when '+'
+        increment
+      when '-'
+        decrement
+      when '.'
+        @out += @ram[@rm].chr
+      when ','
+        @ram[@rm] = @input[@ip].ord
+        @ip += 1
+      when '['
+        getmatch(']', '[') if @ram[@rm] == 0
 
-			when ']'
-				getmatch('[', ']') if @ram[@rm]!= 0
-			end
-			@cp+=1
-		end
-		@out
-	end
+      when ']'
+        getmatch('[', ']') if @ram[@rm]!= 0
+      end
+      @cp+=1
+    end
+    @out
+  end
 
-	def move direction
-		@rm += direction
-	end
+  def move direction
+    @rm += direction
+  end
 
-	def increment
-		@ram[@rm] = 0 if @ram[@rm].nil?
-		@ram[@rm] += 1
-		@ram[@rm] = 0 if @ram[@rm] > 255
-	end
+  def increment
+    @ram[@rm] ||= 0
+    @ram[@rm] += 1
+    @ram[@rm] = 0 if @ram[@rm] > 255
+  end
 
-	def decrement
-		@ram[@rm] = 0 if @ram[@rm].nil?
-		@ram[@rm] -= 1
-		@ram[@rm] = 255 if @ram[@rm] < 0
-	end
+  def decrement
+    @ram[@rm] ||= 0
+    @ram[@rm] -= 1
+    @ram[@rm] = 255 if @ram[@rm] < 0
+  end
 
-	private :getmatch, :increment, :decrement, :move
+  private :getmatch, :increment, :decrement, :move
+
 end
